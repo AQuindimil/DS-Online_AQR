345064209053c9283d83920930ce571538194dae
AQuindimil <aquindimil@outlook.es>
2026-02-06 15:02:36 +0100

    Last_commit
    Aplico las últimas modificaciones y subo nuevos archivos

--- Team_Challenges/TC_03_Sprint_09_ToolBox/temporal/funcion_tipifica_variables.py
+++ Team_Challenges/TC_03_Sprint_09_ToolBox/temporal/funcion_tipifica_variables.py
@@ -13,16 +13,29 @@ Esta sugerencia se hará siguiendo las siguientes pautas:
 
 
 def tipifica_variables(df,umbral_categoria=6, umbral_continua=10):
+    
+    #Precalculamos métricas
+    nombre_variable=df.columns
     Tipo=df.dtypes
     Card=df.nunique()
     Card_rel=Card/len(df)*100
-    df_tipificacion=pd.DataFrame([df.columns, Tipo, Card, Card_rel]).T.rename(columns = {0: "nombre_variable", 1: "Tipo", 2: "Card", 3: "Card_rel"})
-    df_tipificacion["tipo_sugerido"]="sin categoría" 
+    df_tipificacion=pd.DataFrame([nombre_variable, Tipo, Card, Card_rel]).T.rename(columns = {0: "nombre_variable", 1: "Tipo", 2: "Card", 3: "Card_rel"})
+    
+    #Inicializamos columna
+    df_tipificacion["tipo_sugerido"]="Categórica" #Valor por defecto
+    #1. Binaria
     df_tipificacion.loc[df_tipificacion.Card == 2, "tipo_sugerido"] = "Binaria"
+
+    #2. Categórica
+    #Por defecto son categóricas por debajo de umbral_categoria
+
+    #3. Numérica
     es_numerica = ((df_tipificacion.Tipo == "float64") | (df_tipificacion.Tipo == "int64")) 
+    alta_cardinalidad= df_tipificacion["Card"] >= umbral_categoria
     #df_tipificacion.loc[df_tipificacion.Tipo == "datetime64[ns]", "Clasificada_como"] = "Fecha"
-    df_tipificacion.loc[(df_tipificacion.Card > 2) & (df_tipificacion.Card < umbral_categoria), "tipo_sugerido"] = "Categórica"
-    df_tipificacion.loc[(df_tipificacion.Card > umbral_categoria) & es_numerica & (df_tipificacion.Card_rel < umbral_continua), "tipo_sugerido"] = "Numérica discreta"
-    df_tipificacion.loc[(df_tipificacion.Card > umbral_categoria) & es_numerica &  (df_tipificacion.Card_rel > umbral_continua), "tipo_sugerido"] = "Numérica continua"
+    #3a. Numérica discreta
+    df_tipificacion.loc[alta_cardinalidad & es_numerica & (df_tipificacion.Card_rel < umbral_continua), "tipo_sugerido"] = "Numérica discreta"
+    #3b. Numérica continua
+    df_tipificacion.loc[alta_cardinalidad & es_numerica &  (df_tipificacion.Card_rel >= umbral_continua), "tipo_sugerido"] = "Numérica continua"
 
     return df_tipificacion[["nombre_variable", "tipo_sugerido"]]
